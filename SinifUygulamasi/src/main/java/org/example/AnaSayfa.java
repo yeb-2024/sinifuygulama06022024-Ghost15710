592ceeb4a4ef153fd41fa7ae034b57c30fde259e
Mehmet Kemal <isimsizk920@gmail.com>
2025-02-06 18:32:19 +0300

    Anasayfa sınıfı oluşturuldu

---
+++ SinifUygulamasi/src/main/java/org/example/AnaSayfa.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class AnaSayfa {
+    JFrame frame;
+
+    AnaSayfa(){
+        frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
